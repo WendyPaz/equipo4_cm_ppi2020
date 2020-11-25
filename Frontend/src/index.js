151d56ad63d098b5e5658cc23622bffc30c67b81
WendyPaz <66074374+WendyPaz@users.noreply.github.com>
2020-11-24 21:34:01 -0500

    Update index.js

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
-import Aplicacion from "../routes/Aplicacion";
+import Aplicacion from "./Aplicacion.jsx";
 
 ReactDOM.render (
   <>
